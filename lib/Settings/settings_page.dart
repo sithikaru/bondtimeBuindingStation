03c9f96521fd51071c742fd60c11c25a0af95848
Sithija Lakshan Karunasena <sithikaru@gmail.com>
2025-03-23 18:14:08 +0530

    No code changes made; skipping commit.

--- lib/Settings/settings_page.dart
+++ lib/Settings/settings_page.dart
@@ -11,7 +11,6 @@ class SettingsPage extends StatefulWidget {
 }
 
 class SettingsPageState extends State<SettingsPage> {
-  // Toggle states and user data
   bool activityReminders = true;
   bool pushNotifications = false;
   String userFirstName = "User";
@@ -30,6 +29,7 @@ class SettingsPageState extends State<SettingsPage> {
               .collection('users')
               .doc(userId)
               .get();
+
       if (userDoc.exists) {
         var data = userDoc.data() as Map<String, dynamic>;
         setState(() {
@@ -41,7 +41,7 @@ class SettingsPageState extends State<SettingsPage> {
         });
       }
     } catch (e) {
-      // // print("Error loading user settings: $e");
+      // print("Error loading user settings: $e");
     }
   }
 
@@ -55,24 +55,24 @@ class SettingsPageState extends State<SettingsPage> {
         },
       }, SetOptions(merge: true));
     } catch (e) {
-      // // print("Error updating settings: $e");
+      // print("Error updating settings: $e");
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFDFDFD),
+      backgroundColor: const Color(0xFFFDFDFD),
       appBar: AppBar(
-        backgroundColor: Color(0xFFFDFDFD),
+        backgroundColor: const Color(0xFFFDFDFD),
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        title: Align(
+        title: const Align(
           alignment: Alignment.centerLeft,
           child: Text(
             'Settings',
@@ -86,137 +86,12 @@ class SettingsPageState extends State<SettingsPage> {
         ),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // General Settings Card with User Profile Section
-            Container(
-              width: 380,
-              height: 328,
-              decoration: BoxDecoration(
-                color: Color(0xFFFEFEFE),
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(color: Colors.black, width: 1),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Header
-                    Text(
-                      'General Settings',
-                      style: TextStyle(
-                        fontFamily: 'InterTight',
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 15),
-                    // Profile Section
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 49,
-                          backgroundImage: NetworkImage(
-                            'https://via.placeholder.com/150',
-                          ),
-                        ),
-                        SizedBox(width: 15),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              userFirstName, // Fetched user name
-                              style: TextStyle(
-                                fontFamily: 'InterTight',
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              'Edit Your Profile',
-                              style: TextStyle(
-                                fontFamily: 'InterTight',
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Spacer(),
-                        Icon(Icons.edit, color: Colors.black),
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    // Account Settings Section with Toggles
-                    Text(
-                      'Account Settings',
-                      style: TextStyle(fontFamily: 'InterTight', fontSize: 18),
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Activity Reminders',
-                          style: TextStyle(
-                            fontFamily: 'InterTight',
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Switch(
-                          value: activityReminders,
-                          onChanged: (value) {
-                            setState(() {
-                              activityReminders = value;
-                            });
-                            _updateSettings();
-                          },
-                          activeColor: Color(0xFF111111),
-                          activeTrackColor: Color(0xFFC1C1C1),
-                          inactiveThumbColor: Color(0xFF888888),
-                          inactiveTrackColor: Color(0xFFC1C1C1),
-                          materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Push Notifications',
-                          style: TextStyle(
-                            fontFamily: 'InterTight',
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Switch(
-                          value: pushNotifications,
-                          onChanged: (value) {
-                            setState(() {
-                              pushNotifications = value;
-                            });
-                            _updateSettings();
-                          },
-                          activeColor: Color(0xFF111111),
-                          activeTrackColor: Color(0xFFC1C1C1),
-                          inactiveThumbColor: Color(0xFF888888),
-                          inactiveTrackColor: Color(0xFFC1C1C1),
-                          materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 25),
-            // Custom List Tiles for Navigation
+            _buildGeneralSettingsCard(),
+            const SizedBox(height: 25),
             customListTile(
               icon: Icons.workspace_premium,
               title: 'Manage Subscription',
@@ -224,7 +99,7 @@ class SettingsPageState extends State<SettingsPage> {
                 Navigator.pushNamed(context, '/manage_subscription');
               },
             ),
-            SizedBox(height: 25),
+            const SizedBox(height: 25),
             customListTile(
               icon: Icons.security,
               title: 'Security',
@@ -232,7 +107,7 @@ class SettingsPageState extends State<SettingsPage> {
                 Navigator.pushNamed(context, '/security');
               },
             ),
-            SizedBox(height: 25),
+            const SizedBox(height: 25),
             customListTile(
               icon: Icons.message,
               title: 'Contact us',
@@ -240,50 +115,169 @@ class SettingsPageState extends State<SettingsPage> {
                 Navigator.pushNamed(context, '/contact_us');
               },
             ),
-            SizedBox(height: 30),
-            // Logout Button
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: GestureDetector(
-                  onTap: () async {
-                    await FirebaseAuth.instance.signOut();
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    await prefs.setBool('isLoggedIn', false);
-                    await prefs.clear();
+            const SizedBox(height: 30),
+            _buildLogoutButton(),
+          ],
+        ),
+      ),
+    );
+  }
 
-                    if (!mounted)
-                      return; // ✅ ensures context is still safe to use
-
-                    Navigator.pushNamedAndRemoveUntil(
-                      context,
-                      '/sign-in',
-                      (route) => false,
-                    );
-                  },
-
-                  child: Container(
-                    width: 380,
-                    height: 65,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF111111),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Log out',
-                      style: TextStyle(
-                        fontFamily: 'InterTight',
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ),
+  Widget _buildGeneralSettingsCard() {
+    return Container(
+      width: 380,
+      height: 328,
+      decoration: BoxDecoration(
+        color: const Color(0xFFFEFEFE),
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: Colors.black, width: 1),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'General Settings',
+              style: TextStyle(
+                fontFamily: 'InterTight',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
             ),
+            const SizedBox(height: 15),
+            Row(
+              children: [
+                const CircleAvatar(
+                  radius: 49,
+                  backgroundImage: NetworkImage(
+                    'https://via.placeholder.com/150',
+                  ),
+                ),
+                const SizedBox(width: 15),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      userFirstName,
+                      style: const TextStyle(
+                        fontFamily: 'InterTight',
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      'Edit Your Profile',
+                      style: TextStyle(
+                        fontFamily: 'InterTight',
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                const Icon(Icons.edit, color: Colors.black),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Account Settings',
+              style: TextStyle(fontFamily: 'InterTight', fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            _buildSwitchTile(
+              label: 'Activity Reminders',
+              value: activityReminders,
+              onChanged: (value) {
+                setState(() {
+                  activityReminders = value;
+                });
+                _updateSettings();
+              },
+            ),
+            _buildSwitchTile(
+              label: 'Push Notifications',
+              value: pushNotifications,
+              onChanged: (value) {
+                setState(() {
+                  pushNotifications = value;
+                });
+                _updateSettings();
+              },
+            ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSwitchTile({
+    required String label,
+    required bool value,
+    required Function(bool) onChanged,
+  }) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            fontFamily: 'InterTight',
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Switch(
+          value: value,
+          onChanged: onChanged,
+          activeColor: const Color(0xFF111111),
+          activeTrackColor: const Color(0xFFC1C1C1),
+          inactiveThumbColor: const Color(0xFF888888),
+          inactiveTrackColor: const Color(0xFFC1C1C1),
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLogoutButton() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 50),
+        child: GestureDetector(
+          onTap: () async {
+            await FirebaseAuth.instance.signOut();
+            final prefs = await SharedPreferences.getInstance();
+            await prefs.setBool('isLoggedIn', false);
+            await prefs.clear();
+
+            if (!mounted) {
+              return;
+            }
+
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              '/sign-in',
+              (route) => false,
+            );
+          },
+          child: Container(
+            width: 380,
+            height: 65,
+            decoration: BoxDecoration(
+              color: const Color(0xFF111111),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            alignment: Alignment.center,
+            child: const Text(
+              'Log out',
+              style: TextStyle(
+                fontFamily: 'InterTight',
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ),
         ),
       ),
     );
@@ -303,7 +297,7 @@ class SettingsPageState extends State<SettingsPage> {
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(15),
-          border: Border.all(color: Color(0xFF111111), width: 1),
+          border: Border.all(color: const Color(0xFF111111), width: 1),
         ),
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15),
@@ -313,10 +307,10 @@ class SettingsPageState extends State<SettingsPage> {
               Row(
                 children: [
                   Icon(icon, color: Colors.black),
-                  SizedBox(width: 15),
+                  const SizedBox(width: 15),
                   Text(
                     title,
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontFamily: 'InterTight',
                       fontSize: 16,
                       color: Colors.black,
@@ -324,7 +318,7 @@ class SettingsPageState extends State<SettingsPage> {
                   ),
                 ],
               ),
-              Icon(Icons.arrow_forward_ios, color: Colors.black),
+              const Icon(Icons.arrow_forward_ios, color: Colors.black),
             ],
           ),
         ),
