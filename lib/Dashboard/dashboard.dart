c5de3cae3b655e569673f67547ce02a7f4ee848f
Sithija Lakshan Karunasena <sithikaru@gmail.com>
2025-03-23 17:43:52 +0530

    Rename _DashboardScreenState to DashboardScreenState for consistency

--- lib/Dashboard/dashboard.dart
+++ lib/Dashboard/dashboard.dart
@@ -14,10 +14,10 @@ class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
   @override
-  _DashboardScreenState createState() => _DashboardScreenState();
+  DashboardScreenState createState() => DashboardScreenState();
 }
 
-class _DashboardScreenState extends State<DashboardScreen> {
+class DashboardScreenState extends State<DashboardScreen> {
   final PageController _pageController = PageController(initialPage: 0);
   final PageController _tipsPageController = PageController(initialPage: 0);
   int currentPage = 0;
