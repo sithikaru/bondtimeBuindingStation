00bc52651e6d6549b3cd7a1e96d17f3c18593e46
Sithija Lakshan Karunasena <sithikaru@gmail.com>
2025-03-23 19:25:56 +0530

    Update base URL in ApiService for production environment

--- lib/utils/api_service.dart
+++ lib/utils/api_service.dart
@@ -6,7 +6,8 @@ import 'dart:async';
 
 class ApiService {
   // Use 10.0.2.2 for Android emulator to reach the host machine
-  static const String baseUrl = "http://10.0.2.2:8000"; // Updated for emulator
+  static const String baseUrl =
+      "https://bondtime-backend-nodejs2.vercel.app"; // Updated for emulator
 
   // Fetch activities based on the user's ID and today's date
   static Future<Map<String, dynamic>> getActivities(String userId) async {
